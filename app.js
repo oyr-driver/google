5cfe78fa8a8ad48f583f6b3a5e290665a4287ad8
esthel <esthel9026@naver.com>
2022-08-09 01:02:25 +0900

    google draft

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const ejs = require('ejs');
 const app = express();
 const server = http.createServer(app);
 
-const port = 3000;
+const port = 8080;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static("assets"));
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
+
+app.get('/search', (req,res) => {
+    res.render('search');
+});
+
+app.get('/done', (req,res) => {
+    res.render('done');
+});
 
 server.listen(port, () => {
     console.log(`Server running at https://localhost:${port}/`);
